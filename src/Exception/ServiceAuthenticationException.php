7e3ae346da5fcfc201a1eb3d1e0b781d305d0165
Sergei <sergey.shilko@gmail.com>
2024-01-02 21:45:46 +0100

    pages branch

--- src/Exception/ServiceAuthenticationException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types = 1);
-
-namespace myrpc\Exception;
-
-class ServiceAuthenticationException extends ServiceException
-{
-}
--- src/Handler/Context/ContextInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types = 1);
-
-namespace myrpc\Handler\Context;
-
-interface ContextInterface extends ContextWriterInterface, ContextReaderInterface
-{
-}
