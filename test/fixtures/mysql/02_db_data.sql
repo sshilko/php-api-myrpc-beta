b7c73638984b76110424506384adccd6dbbe6b70
Sergei <sergey.shilko@gmail.com>
2024-01-02 21:36:54 +0100

    fix ci, add dummy mysql container

---
+++ test/fixtures/mysql/02_db_data.sql
@@ -0,0 +1,10 @@
+-- This file is part of the sshilko/php-api-myrpc package.
+--
+-- (c) Sergei Shilko <contact@sshilko.com>
+--
+-- MIT License
+--
+-- For the full copyright and license information, please view the LICENSE
+-- file that was distributed with this source code.
+--
+-- @license https://opensource.org/licenses/mit-license.php MIT
